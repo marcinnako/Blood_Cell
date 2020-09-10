bc11dbb552fc0acb5c8c0fb7c4b5eba3b1940c05
marcinnako <43958918+marcinnako@users.noreply.github.com>
2020-09-10 18:56:09 +0200

    Added simple version. ≈50% accuracy

--- data_loaders.jl
+++ data_loaders.jl
@@ -1,11 +1,12 @@
 # This file have all functions
 # that load and batch data
 using Images, Random
+using Flux: onehotbatch
 
 
 function load_data(path::String,perc::Float64,batch::Int64,type::String)
     lebs = ["EOSINOPHIL","LYMPHOCYTE","MONOCYTE","NEUTROPHIL"]
-    im_size = (240,320)
+    im_size = (240,320) #OG
     h::Int64 = 0
     for leb in lebs
         n = Int.(floor(size(readdir(path*"\\"*type*"\\"*leb))[1]*perc))
@@ -13,10 +14,10 @@ function load_data(path::String,perc::Float64,batch::Int64,type::String)
     end
     n_batch::Int64 = Int.(floor(h/batch)) #Number of batches of size var(batch)
     n_add::Int64 = h - n_batch*batch  # Size of last batch
-    X = [(Array{Float32}(undef, 240, 320,3,batch)) for i=1:n_batch]
+    X = [(Array{Float32}(undef, 224, 224,3,batch)) for i=1:n_batch]
     Y = [(Array{String}(undef,batch)) for i=1:n_batch]
     if(n_add!=0)
-        push!(X,(Array{Float32}(undef, 240, 320,3,n_add)))
+        push!(X,(Array{Float32}(undef, 224, 224,3,n_add)))
         push!(Y,(Array{String}(undef,n_add)))
     end
     rng = Random.shuffle(collect(1:h))
@@ -27,14 +28,61 @@ function load_data(path::String,perc::Float64,batch::Int64,type::String)
         for filename in names[1:n]
             idx = rng[i_rng] - 1
             i_rng +=1
-            img = Images.load(path*"\\"*type*"\\"*leb*"\\"*filename)
-            n_batch = Int.(floor(idx/batch) + 1)
+            img = Images.imresize(Images.load(path*"\\"*type*"\\"*leb*"\\"*filename),224,224)
+            nu_batch = Int.(floor(idx/batch) + 1)
             id_batch = idx%batch + 1
-            X[n_batch][:,:,1,id_batch] = Images.red.(img)
-            X[n_batch][:,:,2,id_batch] = Images.green.(img)
-            X[n_batch][:,:,3,id_batch] = Images.blue.(img)
-            Y[n_batch][id_batch] = leb
+            X[nu_batch][:,:,1,id_batch] = Images.red.(img)
+            X[nu_batch][:,:,2,id_batch] = Images.green.(img)
+            X[nu_batch][:,:,3,id_batch] = Images.blue.(img)
+            Y[nu_batch][id_batch] = leb
         end
+    end
+    # OnehotEncoding
+    if(n_add!=0)
+        Y = [onehotbatch(Y[i],lebs) for i=1:n_batch+1]
+    else
+        Y = [onehotbatch(Y[i],lebs) for i=1:n_batch]
+    end
+    X,Y
+end
+function load_data2(path::String,perc::Float64,batch::Int64,type::String)
+    lebs = ["EOSINOPHIL","LYMPHOCYTE","MONOCYTE","NEUTROPHIL"]
+    im_size = (240,320) #OG
+    h::Int64 = 0
+    for leb in lebs
+        n = Int.(floor(size(readdir(path*"\\"*type*"\\"*leb))[1]*perc))
+        h += n
+    end
+    n_batch::Int64 = Int.(floor(h/batch)) #Number of batches of size var(batch)
+    n_add::Int64 = h - n_batch*batch  # Size of last batch
+    X = [(Array{Float32}(undef, 120, 160,3,batch)) for i=1:n_batch]
+    Y = [(Array{String}(undef,batch)) for i=1:n_batch]
+    if(n_add!=0)
+        push!(X,(Array{Float32}(undef, 120, 160,3,n_add)))
+        push!(Y,(Array{String}(undef,n_add)))
+    end
+    rng = Random.shuffle(collect(1:h))
+    i_rng = 1 # var that is used to assign random places in data
+    @time for leb in lebs
+        n = Int.(floor(size(readdir(path*"\\"*type*"\\"*leb))[1]*perc))
+        names = readdir(path*"\\"*type*"\\"*leb)
+        for filename in names[1:n]
+            idx = rng[i_rng] - 1
+            i_rng +=1
+            img = Images.imresize(Images.load(path*"\\"*type*"\\"*leb*"\\"*filename),120,160)
+            nu_batch = Int.(floor(idx/batch) + 1)
+            id_batch = idx%batch + 1
+            X[nu_batch][:,:,1,id_batch] = Images.red.(img)
+            X[nu_batch][:,:,2,id_batch] = Images.green.(img)
+            X[nu_batch][:,:,3,id_batch] = Images.blue.(img)
+            Y[nu_batch][id_batch] = leb
+        end
+    end
+    # OnehotEncoding
+    if(n_add!=0)
+        Y = [onehotbatch(Y[i],lebs) for i=1:n_batch+1]
+    else
+        Y = [onehotbatch(Y[i],lebs) for i=1:n_batch]
     end
     X,Y
 end
--- main.jl
+++ main.jl
@@ -1,8 +1,64 @@
 include("data_loaders.jl")
+using Flux: onecold
+using CuArrays, Flux
+using Statistics
+using Base.Iterators: repeated
 
 wd = "C:\\Users\\lukas\\Downloads\\9232_29380_bundle_archive\\dataset2-master\\dataset2-master\\images"
 n = 1.0
-b_size = 50
+b_size = 140
 
-X,Y= load_data(wd,n,b_size,"TRAIN")
-X_t,Y_t= load_data(wd,n,b_size,"TEST")
+X,Y= load_data2(wd,n,b_size,"TRAIN")
+X_t,Y_t= load_data2(wd,n,b_size,"TEST")
+n_batch = size(X)[1]
+m = Chain(
+    Conv((2,2), 3=>16 ,pad = (1,1),relu),
+    MaxPool((2,2)),
+    Conv((2,2), 16=>8 ,pad = (1,1),relu),
+    MaxPool((2,2)),
+    Conv((2,2), 8=>4 ,pad = (1,1),relu),
+    MaxPool((2,2)),
+    Conv((2,2), 4=>4 ,pad = (1,1),relu),
+    MaxPool((2,2)),
+    x -> flatten(x),
+    Dense(320,32),
+    x -> relu.(x),
+    Dense(32,16),
+    x -> relu.(x),
+    Dense(16,8),
+    x -> relu.(x),
+    Dense(8,4),
+    softmax)
+
+loss(x,y) = Flux.mse(m(x),y)
+opt = ADAM()
+accuracy(x, y) = mean(onecold(m(x)) .== onecold(y))
+function test(X,Y)
+    n,out = size(X)[1], 0.0
+    for i=1:n out += accuracy(X[i],Y[i]) end
+    floor((out/n)*1e3)/1e3
+end
+function ltest(X,Y)
+    n,out = size(X)[1], 0.0
+    for i=1:n out += loss(X[i],Y[i]) end
+    floor((out/n)*1e3)/1e3
+end
+
+@time println("Testing accuracy: ",test(X_t,Y_t))
+m = m |> gpu
+n_epochs = 30
+println("Training Started")
+@time for i=1:n_epochs
+    println("Epoch nr: $i")
+    @time for j=1:n_batch
+        a, b = CuArray(X[j]), CuArray(Y[j])
+        Flux.train!(loss, params(m), repeated((a,b),1), opt)
+        α, β, df = nothing, nothing, nothing
+        a, b = nothing, nothing
+        GC.gc()
+    end
+end
+println("Training ended")
+m = m |> cpu
+@time println("Testing accuracy: ",test(X_t,Y_t))
+@time println("Trening accuracy: ",test(X,Y))
